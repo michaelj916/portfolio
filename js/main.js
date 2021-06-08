a5ffc97a0ed88cefc99c77ef3e363b9b92e6e4ea
michaelj916 <mjarrah916@gmail.com>
2021-06-08 03:26:18 -0700

    styled and added links -- NOTE: add mobile link stylings

--- js/main.js
+++ js/main.js
@@ -1,21 +1,57 @@
-var text = new Blotter.Text("MICHAEL JARRAH", {
-    family : "monospace",
-    size : 35,
-    weight : 950,
-    fill : "#221d1d",
-    paddingLeft: 10,
-    paddingRight: 10
-});
+// check for device size ( 768px targets mobile devices and upwards to iPad's )
+var responsive = window.matchMedia("(max-width: 768px)");
 
+
+if (responsive.matches) {
+    var firstN = new Blotter.Text("MICHAEL JARRAH", {  // if device is responsive, decrease font size and padding
+        family : "'Roboto', sans-serif",
+        size : 40,
+        weight : 900,
+        fill : "#4d4445",
+        paddingLeft: 20,
+        paddingRight: 20,
+        paddingTop: 0,
+        paddingBottom: 0
+    });
+}
+else {
+    var firstN = new Blotter.Text("MICHAEL JARRAH", {  // else increase font size and padding for computers
+        family : "arial",
+        size : 75,
+        weight : 950,
+        fill : "#4d4445",
+        paddingLeft: 50,
+        paddingRight: 50,
+        paddingTop: 30,
+        paddingBottom: 30
+    });
+}
+
+// refer 'https://blotter.js.org/#/documentation'
 var material = new Blotter.LiquidDistortMaterial();
 
-material.uniforms.uSpeed.value = 0.25;
+if (responsive.matches) {
+    material.uniforms.uSpeed.value = 0.2;
+    material.uniforms.uVolatility.value = 0.05;
+    material.uniforms.uSeed.value = 0.5;
+}
+else {
+    material.uniforms.uSpeed.value = 0.2;
+    material.uniforms.uVolatility.value = 0.1;
+    material.uniforms.uSeed.value = 0.0;
+}
+
 
 var blotter = new Blotter(material, { 
-    texts : text
+    texts : firstN
 });
 
 var elem = document.getElementById("name");
-var scope = blotter.forText(text);
+var scope = blotter.forText(firstN);
 
 scope.appendTo(elem);
+
+
+
+/* --------------------------------------------------------------------------------------- */
+
